e20e1217a5dabdb3c7e52013916f87cf5954ac62
Raghav Ramesh <raghavramesh94@gmail.com>
2017-08-13 12:03:16 +0800

    Style + Displayed more details for each video. + Created and imported an scss stylesheet to style the video details.

--- src/js/components/SimpleSlider.js
+++ src/js/components/SimpleSlider.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import Slider from 'react-slick';
+import style from '../../sass/styles.scss';
 
 export default class SimpleSlider extends React.Component {
 	constructor(props) {
@@ -48,10 +49,43 @@ export default class SimpleSlider extends React.Component {
 
 		let slides = videos.map(video => {
 			return (
-				<div key={video.id.toString()} >
-					<img src={video.images[0].url}/>
-					<div>{video.title}</div>
-					<div>{video.description}</div>
+				<div key={video.id.toString()} class="slide-item" >
+					<img src={video.images[0].url} />
+					<div class="details">
+						<h4 class="title">{video.title}</h4>
+						<p class="sub-title">
+							<span class="year">{new Date(video.publishedDate).getFullYear()}</span>
+							<span class="lang">{video.metadata[0].value.toUpperCase()}</span>
+						</p>
+						<p class="description">{video.description}</p>
+						<table class="more-details">
+							<tbody>
+								<tr>
+									<td class="field">Director</td>
+									<td class="value">{video.credits[0].name}</td>
+								</tr>
+								<tr>
+									<td class="field">Cast</td>
+									<td class="value">
+									{
+										video.credits.map((c, i, a) => {
+											if (i > 0) {
+												if (i < a.length - 1)
+													return c.name + ', ';
+												else
+													return c.name;
+											}
+										})
+								  }
+								  </td>
+								</tr>
+								<tr>
+									<td class="field">Rating</td>
+									<td class="value">{video.parentalRatings[0].rating}</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			);
 		});
